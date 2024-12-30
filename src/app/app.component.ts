125fdcc97bf970582a9f7db38a83c310af328eec
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2024-12-30 17:42:12 +0600

    Adds fontawesome package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faStackOverflow,
+  faGithub,
+  faMedium,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, FontAwesomeModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'rshossain';
+  
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faStackOverflow,
+      faGithub,
+      faMedium
+    );
+  }
 }
