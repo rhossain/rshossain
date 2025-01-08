b230165520895850ad32a20e80d0861831fca4ca
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2025-01-08 17:08:40 +0600

    fix: refactors style codebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: withOpacity("--color-primary"),
+        'primary-1': withOpacity("--color-primary-1"),
+        'brand-facebook': withOpacity("--color-brand-facebook"),
+        'brand-google': withOpacity("--color-brand-google"),
+        'brand-instagram': withOpacity("--color-brand-instagram"),
+        'brand-linkedin': withOpacity("--color-brand-linkedin"),
+        'brand-pinterest': withOpacity("--color-brand-pinterest"),
+        'brand-skype': withOpacity("--color-brand-skype"),
+        'brand-twitter': withOpacity("--color-brand-twitter"),
       },
-    },
-  },
-  variants: {
-    opacity: ({ after }) => after(["disabled"]),
-    extend: {
-      backgroundColor: ["active"],
-      outline: ["focus"],
     },
   },
   plugins: [],
