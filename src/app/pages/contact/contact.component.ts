813e2b6f4dbb55aaa1e95acddf60493cb316e1e1
Md. Sazzad Hossain <sazzad.hossain@bs-23.net>
2025-02-23 12:54:57 +0600

    feat: adds send mail form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,48 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { TextareaModule } from 'primeng/textarea';
+import emailjs from '@emailjs/browser';
 import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule, FloatLabelModule, TextareaModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   themeService: ThemeService = inject(ThemeService);
+
+  contactForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    msg: new FormControl("", [Validators.required]),
+  })
+
+  ngOnInit() {
+    
+  }
+
+  onSubmit() {
+    const formValue = this.contactForm.value;
+    console.log(formValue);
+
+    emailjs.send(
+      'service_l8932p9', 
+      'template_dhlosfx', 
+      { ...this.contactForm }, 
+      {
+        publicKey: 'twye9DbxkMuiig85C'
+      }
+      )
+      .then(
+      () => {
+        console.log('Sent!');
+      },
+      (err) => {
+        console.log('FAILED...', err);
+      },
+    );
+  }
 }
